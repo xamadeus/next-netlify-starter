6cc67a08118180955a063fc87204ebe7f99c2e9c
Niom <amadou-lamine.niom@cgi.com>
2021-05-11 16:33:36 +0200

    Correction

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,7 @@ export default function Home() {
         <title>W3.CSS</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<style>
-.mySlides {display:none;}
-</style>
       </Head>
-
       <main>
        <h2 class="w3-center">Manual Slideshow</h2>
 
